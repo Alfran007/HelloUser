6850bc990099892cccb01c837f254b81a095a474
Alfran007 <alfran.rockstar@gmail.com>
2017-06-16 23:53:17 +0530

    Google SignIn updated

---
+++ app/src/main/java/com/eternalapps/hello/hellouser/CodelabPreferences.java
@@ -0,0 +1,12 @@
+
+package com.eternalapps.hello.hellouser;
+
+/**
+ * Created by alfran on 6/16/2017.
+ */
+
+public class CodelabPreferences {
+
+    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
+
+}
